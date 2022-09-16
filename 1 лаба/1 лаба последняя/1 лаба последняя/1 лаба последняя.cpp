0e7edf0ee21b8e829dc28362347dff4da1ba231e
Prokofev-Nikita <112731066+Prokofev-Nikita@users.noreply.github.com>
2022-09-16 11:31:05 +0300

    Add files via upload

---
+++ 1 лаба/1 лаба последняя/1 лаба последняя/1 лаба последняя.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+	
+	char* eurasia[] = { "Russia", "Belarus", "France", "Great Britan", "Germany", "Poland", "Finland","China" };
+	int a;
+	printf("dfgdgfdg\n");
+	scanf_s("%d\n", &a);
+	printf("Num:%s", eurasia[n-1]);
+	 /*
+	 char* namr[] = ("USA", "Panama", "Mexico", "Canada")
+		char* samr[] = ("",);
+	 */
+	return 0;
+}
+
+
